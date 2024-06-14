9581a8b5bcc94c7b3a8230e3c9860d76dd956dd4
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2024-06-15 01:22:05 +0300

    Added test cases to App.c

--- App.c
+++ App.c
@@ -1,29 +1,100 @@
-#include "dynamicArray.h"
+#include "DynamicMemory.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
-    Array *array = malloc(sizeof(Array));
-    InitArray(array);
+typedef struct{
+    int size;
+    int item_size;
+}Testi;
 
-    printf("Push\n");
-
-    Push(9, array);
-    Push(15, array);
-    Push(18, array);
-    int i = 0;
-    for(i = 0; i < array->size; i++){
-        printf("%d\n", array->arr[i]);
+int main(void)
+{
+    List *list = Create(sizeof(int*));
+    void *ptr = malloc(list->item_size);
+    *((int*)ptr) = 5;
+    Push(list, ptr);
+    Push(list, ptr);
+    Push(list, ptr);
+    Push(list, ptr);
+    for (size_t i = 0; i < list->size; i++)
+    {
+        void* ptrs = list->memory[i];
+        printf("%d\n", *((int*)ptrs));
     }
-    
-    printf("\nRelease\n");
+    printf("--------\n");
+    List *lista = Create(sizeof(char**));
+    void *stringPtr = malloc(lista->item_size);
+    *((char**)stringPtr) = "Xdd";
 
-    Release(array, 3);
-    Release(array, 2);
 
-    int j = 0;
-    for(j = 0; j < array->size; j++){
-        printf("%d\n", array->arr[j]);
+    void *stringPtr1 = malloc(lista->item_size);
+    *((char**)stringPtr1) = "Moi";
+
+
+    Push(lista, stringPtr);
+    Push(lista, stringPtr1);
+    Push(lista, stringPtr);
+    Push(lista, stringPtr1);
+    Push(lista, stringPtr);
+    Push(lista, stringPtr1);
+    Push(lista, stringPtr);
+    Push(lista, stringPtr1);
+    for (size_t i = 0; i < lista->size; i++)
+    {
+        void* ptrs = lista->memory[i];
+        printf("%s\n", *((char**)ptrs));
+    }    
+    printf("%d\n", lista->size);
+    printf("--------\n");
+
+    Release(lista, 1);
+    Release(lista, 2);
+    Release(lista, 0);
+
+    for (size_t i = 0; i < lista->size; i++)
+    {
+        void* ptrs = lista->memory[i];
+        printf("%s\n", *((char**)ptrs));
     }
-    printf("Amount of items: %d", array->size);
+    printf("%d\n", lista->size);
+
+    printf("--------\n");
+
+    for (size_t i = 0; i < 8; i++)
+    {
+        void* ptrs = lista->memory[i];
+        printf("%p\n", ptrs);
+    }
+
+    printf("--------\n");
+
+    Release(lista, 1);
+    Release(lista, 2);
+    Release(lista, 0);
+
+    for (size_t i = 0; i < 8; i++)
+    {
+        void* ptrs = lista->memory[i];
+        printf("%p\n", ptrs);
+    }
+
+
+    printf("--------\n");
+    List *testiLista = Create(sizeof(Testi*));
+    void *testiPtr = malloc(testiLista->item_size);
+    Testi *my_struct;
+    my_struct = malloc(sizeof(Testi));
+    my_struct->item_size = 676;
+    my_struct->size = 6969;
+    *((Testi*)testiPtr) = *my_struct;
+
+    Push(testiLista, testiPtr);
+
+    for (size_t i = 0; i < 1; i++)
+    {
+        void* ptrs = testiLista->memory[i];
+        Testi value1 = *((Testi*)ptrs);
+        printf("%d,, %d\n", value1.item_size, value1.size);
+    }
+
+    return 0;
 }
